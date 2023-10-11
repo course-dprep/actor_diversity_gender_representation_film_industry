82f40755aea9ae8173d1b3890cff8bcdb7e4bbf6
Stan van Goor <stanvangoor@ww120124.uvt.nl>
2023-10-11 15:48:29 +0200

    Final adjustment. Corrected wrong pathnames in execute rmd files

--- src/paper/Execute_titlebasics-Rmd.R
+++ src/paper/Execute_titlebasics-Rmd.R
@@ -1,6 +1,6 @@
 rmarkdown::render('../data-preparation/Data_RMD_files/IMDB_dataset_title_basics.Rmd')
 
-file.copy(from = paste0("../../src/data-preparation/IMDB_dataset_title_basics.pdf"),
+file.copy(from = paste0("../data-preparation/Data_RMD_files/IMDB_dataset_title_basics.pdf"),
           to = paste0("../../gen/data-preparation/output/IMDB_dataset_title_basics.pdf"))
 
-file.remove(from = paste0("../../src/data-preparation/IMDB_dataset_title_basics.pdf"))
+file.remove(from = paste0("../data-preparation/Data_RMD_files/IMDB_dataset_title_basics.pdf"))
